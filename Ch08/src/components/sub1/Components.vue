d0dcffdd3c3f7a52363ef39bb189415434772ed6
soungjun <jungy0309@naver.com>
2023-04-14 18:39:17 +0900

    2023.04.14 Vuetify 실습

---
+++ Ch08/src/components/sub1/Components.vue
@@ -0,0 +1,61 @@
+<template>
+  <h4>Vuetify Components 실습</h4>
+
+  <div>
+    <h4>Icons</h4>
+    <v-icon icon="mdi-vuetify"></v-icon>
+    <v-icon color="success" icon="mdi-wifi" size="large"></v-icon>
+    <v-icon icon="mdi-dog" color="info"></v-icon>
+    <hr />
+
+    <h4>Images</h4>
+    <v-img
+      :width="300"
+      :height="200"
+      aspect-ratio="16/9"
+      cover
+      src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+    ></v-img>
+    <hr />
+
+    <h4>Button</h4>
+    <v-btn> Button1 </v-btn>
+    <v-btn prepend-icon="mdi-cat"> Button2 </v-btn>
+    <v-btn block>Block Button3</v-btn>
+    <v-btn color="primary"> Button4 </v-btn>
+    <v-btn color="secondary"> Button5 </v-btn>
+    <v-btn color="success"> Button6 </v-btn>
+    <v-btn color="warning"> Button6 </v-btn>
+    <hr />
+
+    <h4>Cards</h4>
+    <v-card title="제목입니다." subtitle="부제목입니다." text="내용입니다.">
+      <v-card-actions>
+        <v-btn>확인</v-btn>
+      </v-card-actions>
+    </v-card>
+
+    <v-card
+      width="400"
+      title="제목입니다."
+      subtitle="부제목입니다."
+      text="내용입니다."
+      class="mt-2"
+      variant="outlinde"
+    >
+      <v-card-actions>
+        <v-btn>확인</v-btn>
+      </v-card-actions>
+    </v-card>
+
+    <v-card width="400">
+      <v-card-actions>
+        <v-btn>확인</v-btn>
+      </v-card-actions>
+    </v-card>
+
+    <hr />
+  </div>
+</template>
+<script setup></script>
+<style scoped></style>
