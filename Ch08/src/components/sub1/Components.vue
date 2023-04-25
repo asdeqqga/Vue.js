dbe38507f0274fc5fbeb912961563bceb8e722ba
soungjun <jungy0309@naver.com>
2023-04-25 19:30:24 +0900

    2023.04.12 // Vue.js 실습

--- Ch08/src/components/sub1/Components.vue
+++ Ch08/src/components/sub1/Components.vue
@@ -2,60 +2,49 @@
   <h4>Vuetify Components 실습</h4>
 
   <div>
-    <h4>Icons</h4>
-    <v-icon icon="mdi-vuetify"></v-icon>
-    <v-icon color="success" icon="mdi-wifi" size="large"></v-icon>
-    <v-icon icon="mdi-dog" color="info"></v-icon>
+    <h5>Icons</h5>
+    <v-icon color="info" icon="mdi-alert-circle" size="x-small"></v-icon>
     <hr />
 
-    <h4>Images</h4>
+    <h5>Images</h5>
     <v-img
       :width="300"
-      :height="200"
       aspect-ratio="16/9"
       cover
       src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
     ></v-img>
     <hr />
 
-    <h4>Button</h4>
-    <v-btn> Button1 </v-btn>
-    <v-btn prepend-icon="mdi-cat"> Button2 </v-btn>
-    <v-btn block>Block Button3</v-btn>
-    <v-btn color="primary"> Button4 </v-btn>
-    <v-btn color="secondary"> Button5 </v-btn>
-    <v-btn color="success"> Button6 </v-btn>
-    <v-btn color="warning"> Button6 </v-btn>
+    <h5>Buttons</h5>
+    <v-btn variant="outlined" rounded="xl" color="blue"> Button </v-btn>
     <hr />
 
-    <h4>Cards</h4>
-    <v-card title="제목입니다." subtitle="부제목입니다." text="내용입니다.">
+    <h5>Cards</h5>
+    <v-card :width="400" color="#fff0f5" class="border my-2">
+      <v-img
+        height="200"
+        src="https://cdn.pixabay.com/photo/2020/07/12/07/47/bee-5396362_1280.jpg"
+        cover
+        class="text-white"
+      >
+        <v-toolbar color="rgba(0, 0, 0, 0)" theme="dark">
+          <v-card-item>
+            <v-card-title>제목</v-card-title>
+            <v-card-subtitle>부 제목</v-card-subtitle>
+          </v-card-item>
+        </v-toolbar>
+      </v-img>
+
+      <v-card-text> 내용 </v-card-text>
+
       <v-card-actions>
-        <v-btn>확인</v-btn>
+        <v-btn variant="outlined" color="blue">확인</v-btn>
       </v-card-actions>
     </v-card>
-
-    <v-card
-      width="400"
-      title="제목입니다."
-      subtitle="부제목입니다."
-      text="내용입니다."
-      class="mt-2"
-      variant="outlinde"
-    >
-      <v-card-actions>
-        <v-btn>확인</v-btn>
-      </v-card-actions>
-    </v-card>
-
-    <v-card width="400">
-      <v-card-actions>
-        <v-btn>확인</v-btn>
-      </v-card-actions>
-    </v-card>
-
     <hr />
   </div>
 </template>
+
 <script setup></script>
+
 <style scoped></style>
--- Ch08/src/components/sub3/Layout.vue
+++ Ch08/src/components/sub3/Layout.vue
@@ -2,12 +2,26 @@
   <h4>Vuetify Layout 실습</h4>
 
   <v-app>
-    <v-app-bar> 헤더영역 </v-app-bar>
+    <v-app-bar>
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+
+      <v-app-bar-title>헤더 영역</v-app-bar-title>
+
+      <template v-slot:append>
+        <v-btn icon="mdi-heart"></v-btn>
+        <v-btn icon="mdi-magnify"></v-btn>
+        <v-btn icon="mdi-dots-vertical"></v-btn>
+      </template>
+    </v-app-bar>
     <v-main>
-      <v-container>메인영역</v-container>
+      <v-container>메인 영역</v-container>
     </v-main>
-    <v-footer app color="primary">푸터영역</v-footer>
+    <v-footer app color="blue">푸터 영역</v-footer>
   </v-app>
 </template>
+
 <script setup></script>
+
 <style scoped></style>
